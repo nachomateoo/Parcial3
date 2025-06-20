2e7fc0f6cd7d1db76eeedb34d4eda6cd5a4dba9e
nachomateoo <igalian9@alu.fpsanantonio.com>
2025-06-20 09:09:12 +0200

    Encapsulados los atributos de Empleado y añadidos getters y setters para mejorar la seguridad y el acceso controlado.

--- Empleado.java
+++ Empleado.java
@@ -1,11 +1,35 @@
-class Empleado {
-    String nombre;
-    String cargo;
-    double salario;
+public class Empleado {
+    private String nombre;
+    private String puesto;
+    private double salario;
 
-    public Empleado(String nombre, String cargo, double salario) {
+    public Empleado(String nombre, String puesto, double salario) {
         this.nombre = nombre;
-        this.cargo = cargo;
+        this.puesto = puesto;
+        this.salario = salario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
         this.salario = salario;
     }
 
@@ -13,7 +37,7 @@ class Empleado {
     public String toString() {
         return "Empleado{" +
                 "nombre='" + nombre + '\'' +
-                ", cargo='" + cargo + '\'' +
+                ", puesto='" + puesto + '\'' +
                 ", salario=" + salario +
                 '}';
     }
